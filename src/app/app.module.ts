cc3d55e72e27e54b3b808bf91f88201f8c192516
anurag4one <agresiveanurag4one@gmail.com>
2019-11-05 18:01:19 +0530

    angular some part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
+import { ModifyComponent } from './modify/modify.component';
+import { DeleteComponent } from './delete/delete.component';
+import { NaviRoutingModule } from './navi/navi-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    SearchComponent,
+    ListComponent,
+    ModifyComponent,
+    DeleteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NaviRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
